c731ee55902bdc0c2b10b3aff573618a7d1ae8ca
Zarin <zareenmujam@gmail.com>
2025-12-23 15:37:14 +0530

    Fix router structure for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Message from "./pages/Message";
 import CakePage from "./pages/CakePage";
@@ -16,15 +17,19 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/message" element={<Message />} />
-          <Route path="/cake" element={<CakePage />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+
+      <Routes>
+        {/* HOME */}
+        <Route index element={<Index />} />
+
+        {/* PAGES */}
+        <Route path="message" element={<Message />} />
+        <Route path="cake" element={<CakePage />} />
+        <Route path="gallery" element={<Gallery />} />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
